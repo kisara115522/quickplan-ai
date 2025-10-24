9e81dfe2b6f02efd984d7e890bb6d018b07d4170
kisara115522 <Kisara115522@gmail.com>
2025-10-24 13:28:58 +0800

    初始

---
+++ src/main/java/com/example/quickplan_ai/controller/Aicontroller.java
@@ -0,0 +1,9 @@
+package com.example.quickplan_ai.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+//111
+@RestController
+public class Aicontroller {
+
+
+}
---
+++ src/test/java/com/example/quickplan_ai/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickplan_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
